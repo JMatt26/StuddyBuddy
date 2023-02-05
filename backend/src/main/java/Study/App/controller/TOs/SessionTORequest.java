d6b772478b92e316783a00d9a293ab41e183920d
JMatt26 <jared.matthews@mail.mcgill.ca>
2023-02-04 21:22:45 -0500

    Done checklist in excel format

--- backend/src/main/java/Study/App/controller/TOs/SessionTORequest.java
+++ backend/src/main/java/Study/App/controller/TOs/SessionTORequest.java
@@ -1,5 +1,26 @@
 package Study.App.controller.TOs;
 
 public class SessionTORequest {
-    
+    public Integer sessionId;
+    public Boolean ispublic;
+    public String title;
+    public Integer capacity;
+    public String description;
+    public Integer attendees;
+    public Integer participantId;
+    public Integer sessionInformationId;
+
+    public SessionTORequest(Integer sessionId, Boolean ispublic, String title, Integer capacity, String description, Integer attendees, Integer participantId, Integer sessionInformationId) {
+        this.sessionId = sessionId;
+        this.ispublic = ispublic;
+        this.title = title;
+        this.capacity = capacity;
+        this.description = description;
+        this.attendees = attendees;
+        this.participantId = participantId;
+        this.sessionInformationId = sessionInformationId;
+    }
+
+    public SessionTORequest() {
+    }
 }
