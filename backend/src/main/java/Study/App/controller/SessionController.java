e8f5109954a412ccf5deabaeddc1f49a424a8cfb
Nicholas Murphy <nicholas.murphy2@mail.mcgill.ca>
2023-02-07 14:37:18 -0500

    /joinsession securitycontextholder

--- backend/src/main/java/Study/App/controller/SessionController.java
+++ backend/src/main/java/Study/App/controller/SessionController.java
@@ -4,6 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -56,6 +57,6 @@ public class SessionController {
 
     @PutMapping("/joinSession")
     public void joinSession(@RequestParam Integer sessionId, @RequestParam Integer userId) {
-    
+        String username = SecurityContextHolder.getContext().getAuthentication().getName();
     }
 }
