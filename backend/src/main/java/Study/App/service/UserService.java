be53d414dc38f47cb0583d340c8c2593a861c684
Parsa Langari <parsa.langari@mail.mcgill.ca>
2023-02-07 13:02:45 -0500

    Init

---
+++ backend/src/main/java/Study/App/service/UserService.java
@@ -0,0 +1,23 @@
+package Study.App.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    
+
+
+    public UserService() {
+
+    }
+
+    /**
+     * Creates user using sign up.
+     * @param username
+     * @param password
+     * @param name
+     */
+    public void signUpUser(String username, String password, String name){
+        
+    }
+}
