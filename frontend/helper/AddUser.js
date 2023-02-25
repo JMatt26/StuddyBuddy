09fc1742a3324d1dc85cced7ffdf39b45fd1b765
smahm01 <sadek.mahmood@mail.mcgill.ca>
2023-02-25 13:26:45 -0500

    fixed helper

--- frontend/helper/AddUser.js
+++ frontend/helper/AddUser.js
@@ -1,12 +1,33 @@
 import axios from "axios";
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-export function AddUser(sessionId) {
-  axios
-    .post("http://localhost:8080/joinSession", { sessionId: sessionId })
-    .then(function (response) {
-      console.log(response);
+export async function AddUser(sessionId) {
+
+  let jwt = await AsyncStorage.getItem("userToken");
+
+  const headers = {
+    'Content-Type': 'application/json',
+    'Authorization': `Bearer ${jwt}`
+  }
+  
+  const res = await axios.post("http://localhost:8080/joinSession" + `?sessionId=${sessionId}`, {}, {
+      headers: headers
     })
-    .catch(function (error) {
-      console.log(error);
-    });
+
+  if (res.status == 401) {
+    console.log("JWT Token Expired")
+  } else if (res.status == 200) {
+    console.log("All Good")
+  } else {
+    console.log(res.status)
+  }
+
+  // axios
+  //   .post("http://localhost:8080/joinSession", null, { params: {sessionId}})
+  //   .then(function (response) {
+  //     console.log(response);
+  //   })
+  //   .catch(function (error) {
+  //     console.log(error);
+  //   });
 }
--- frontend/src/Dev.js
+++ frontend/src/Dev.js
@@ -1,7 +1,13 @@
-import { View, Text } from "react-native"
+import { View, Text, Pressable } from "react-native"
 import Categories from "../components/Categories"
 import CategoryButton from "../components/CategoryButton"
+import { AddUser } from "../helper/AddUser"
 export default function Dev(){
+    let test = async () => {
+        console.log("Hello")
+        await AddUser(0)
+    }
+
     return(<View>
         <Text style={{ fontSize: 16, fontWeight: "bold", alignSelf: "center" }}>
             ! FOR DEVELOPMENT ONLY !
@@ -9,5 +15,6 @@ export default function Dev(){
         <Categories />
         <Categories />
         <Categories />
+        <Pressable onPress={() => test()}><Text>Test</Text></Pressable>
     </View>)
 }
