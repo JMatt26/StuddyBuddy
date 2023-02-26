fbfb126d6ecaf8671bfd70a820b9fffb1cd1e20c
Parsa Langari <parsa.langari@mail.mcgill.ca>
2023-02-26 00:04:23 -0500

    Endpoint test template done

--- backend/src/test/java/Study/App/controllers/SessionControllerTest.java
+++ backend/src/test/java/Study/App/controllers/SessionControllerTest.java
@@ -3,22 +3,28 @@ package Study.App.controllers;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+import java.util.List;
+
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.session.SessionRegistry;
 
+import Study.App.controller.TOs.SessionTO;
 import Study.App.model.Session;
 import Study.App.repository.SessionRepository;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class SessionControllerTest {
 
@@ -43,12 +49,31 @@ public class SessionControllerTest {
         session.setDescription("description");
         session.setPrivate(false);
         session.setTitle("title");
+        sessionRepository.save(session);
     }
 
-    @Test
-	public void testCreateAndGetSession() {
+    @Test 
+    public void testGetSessionByName() {
+        // STEP 1: Creating headers that contain the brearer token
+        HttpHeaders headers = new HttpHeaders();
+		headers.set("Authorization", "Bearer " + token);
 
-	}
+        // STEP 2: Creating the request with above headers 
+		HttpEntity req = new HttpEntity(headers);
+
+        // STEP 3: Sending the request to the server to correct URL: note that url contains the reqest parameters
+        ResponseEntity<List<SessionTO>> response = client.exchange(
+            "/session/getAllSessionsBySessionName?sessionName=title", 
+            HttpMethod.GET, 
+            req, 
+            // side note: this is a how you tell the compiler what the type T of the response object is, you wrap it in a new ParameterizedTypeReference<T>() {}
+            new ParameterizedTypeReference<List<SessionTO>>() {}
+        );
+        assertNotNull(response);
+        assertEquals(HttpStatus.OK, response.getStatusCode(), "Response has correct status");
+        assertNotNull(response.getBody(), "Response has body");
+        assertEquals("title", response.getBody().get(0).title, "title");
+    }
 
     private String getToken() {
 		HttpHeaders headers = new HttpHeaders();
