9648d0c23791aef3a13ce008bead0a707f8e9b09
Nicholas Murphy <nicholas.murphy2@mail.mcgill.ca>
2023-02-25 23:14:31 -0500

    beforeall fix

--- backend/src/test/java/Study/App/controllers/SessionControllerTest.java
+++ backend/src/test/java/Study/App/controllers/SessionControllerTest.java
@@ -6,6 +6,7 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
@@ -20,6 +21,7 @@ import Study.App.controller.TOs.SessionTO;
 import Study.App.model.Session;
 import Study.App.repository.SessionRepository;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class SessionControllerTest {
 
