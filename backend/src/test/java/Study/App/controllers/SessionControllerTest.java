ec44c1b18e151a1583b6d06c64a18148eca2ea9e
Parsa Langari <parsa.langari@mail.mcgill.ca>
2023-02-25 22:18:08 -0500

    starter code for endpoint testing

--- backend/src/test/java/Study/App/controllers/SessionControllerTest.java
+++ backend/src/test/java/Study/App/controllers/SessionControllerTest.java
@@ -3,6 +3,8 @@ package Study.App.controllers;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -12,38 +14,47 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.session.SessionRegistry;
 
-import Study.App.controller.TOs.UserTO;
+import Study.App.model.Session;
+import Study.App.repository.SessionRepository;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
-public class AuthControllerTest {
+public class SessionControllerTest {
 
     @Autowired
 	private TestRestTemplate client;
 
+    @Autowired
+    private SessionRepository sessionRepository;
+
+    private static String token;
+    
+    @BeforeAll
+    public void setup() {
+        String token = getToken();
+        SessionControllerTest.token = token;
+    }
+
+    @BeforeEach
+    public void setupEach() {
+        Session session = new Session();
+        session.setCapacity(69);
+        session.setDescription("description");
+        session.setPrivate(false);
+        session.setTitle("title");
+    }
+
     @Test
-	public void testCreateAndGetUser() {
-		testCreatePerson();
-		testGetPerson();
+	public void testCreateAndGetSession() {
+
 	}
 
-    private UserTO testCreatePerson() {
-		ResponseEntity<UserTO> response = client.postForEntity("/signup/", new UserTO(null, "Fouseytube", "fousey", "pass"), UserTO.class);
-		assertNotNull(response);
-		assertEquals(HttpStatus.OK, response.getStatusCode(), "Response has correct status");
-		assertNotNull(response.getBody(), "Response has body");
-		assertEquals("Fouseytube", response.getBody().name, "Response has correct name");
-		System.out.println("Finished create person test");
-		return response.getBody();
-	}
-
-	private void testGetPerson() {
+    private String getToken() {
 		HttpHeaders headers = new HttpHeaders();
 		headers.setBasicAuth("parsa", "pass");
 		HttpEntity req = new HttpEntity(headers);
         ResponseEntity<String> response = client.postForEntity("/login", req, String.class);
-        System.out.println(response);
-        assertNotNull(response.getBody());
-		assertEquals(response.getStatusCode(), HttpStatus.OK);
+        return response.getBody();
 	}
 }
