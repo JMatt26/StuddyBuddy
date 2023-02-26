d48a3a0ecd347ffddef526158bec71482d4efe86
Nicholas Murphy <nicholas.murphy2@mail.mcgill.ca>
2023-02-26 09:57:25 -0500

    small change

--- backend/src/test/java/Study/App/controllers/SessionControllerTest.java
+++ backend/src/test/java/Study/App/controllers/SessionControllerTest.java
@@ -15,7 +15,6 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.session.SessionRegistry;
 
 import Study.App.controller.TOs.SessionTO;
 import Study.App.model.Session;
