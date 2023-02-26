c8b62e821c0c8cdaaca58903df4047e1c649866c
Parsa Langari <parsa.langari@mail.mcgill.ca>
2023-02-25 22:03:02 -0500

    AuthController tests

--- backend/src/test/java/Study/App/controllers/AuthControllerTest.java
+++ backend/src/test/java/Study/App/controllers/AuthControllerTest.java
@@ -2,39 +2,24 @@ package Study.App.controllers;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
 
 import Study.App.controller.TOs.UserTO;
-import Study.App.repository.UserRepository;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class AuthControllerTest {
 
     @Autowired
 	private TestRestTemplate client;
-
-	@Autowired
-	private UserRepository userRepository;
-
-    @BeforeAll
-	@AfterAll
-	public void clearDatabase() {
-		userRepository.deleteAll();
-	}
 
     @Test
 	public void testCreateAndGetUser() {
@@ -46,22 +31,20 @@ public class AuthControllerTest {
 		ResponseEntity<UserTO> response = client.postForEntity("/signup/", new UserTO(null, "Fouseytube", "fousey", "pass"), UserTO.class);
 
 		assertNotNull(response);
-		assertEquals(HttpStatus.CREATED, response.getStatusCode(), "Response has correct status");
+		assertEquals(HttpStatus.OK, response.getStatusCode(), "Response has correct status");
 		assertNotNull(response.getBody(), "Response has body");
 		assertEquals("Fouseytube", response.getBody().name, "Response has correct name");
-
+		System.out.println("Finished create person test");
 		return response.getBody();
 	}
 
 	private void testGetPerson() {
-		MultiValueMap<String, String> request = new LinkedMultiValueMap<String, String>();
-        request.set("username", "parsa");
-        request.set("password", "pass");
-        request.set("grant_type", "password");
-        @SuppressWarnings("unchecked")
-        String response = this.client.withBasicAuth("parsa", "password")
-            .postForObject("/login", request, String.class);
-        
-        assertNotNull(response);
+		HttpHeaders headers = new HttpHeaders();
+		headers.setBasicAuth("parsa", "pass");
+		HttpEntity req = new HttpEntity(headers);
+        ResponseEntity<String> response = client.postForEntity("/login", req, String.class);
+        System.out.println(response);
+        assertNotNull(response.getBody());
+		assertEquals(response.getStatusCode(), HttpStatus.OK);
 	}
 }
