be0cb9231b9bf7d9ded6c2b045aa4f52f0a4f402
JMatt26 <jared.matthews@mail.mcgill.ca>
2023-03-26 12:54:36 -0400

    Rendering in progress

--- frontend/src/pages/Dev.js
+++ frontend/src/pages/Dev.js
@@ -1,19 +1,65 @@
-import { View, Text } from "react-native"
-import ActiveSessions from "../../components/ActiveSessions"
-import Categories from "../../components/Categories"
-import CategoryButton from "../../components/CategoryButton"
-import { Pressable } from "react-native"
-import { AddUser } from "../../utils/AddUser"
-import StudySessionCard from "../../components/StudySessionCard"
-import assetsObject from "../../assets/assets"
+import { View, Text } from "react-native";
+import ActiveSessions from "../../components/ActiveSessions";
+import Categories from "../../components/Categories";
+import CategoryButton from "../../components/CategoryButton";
+import { Pressable } from "react-native";
+import { AddUser } from "../../utils/AddUser";
+import StudySessionCard from "../../components/StudySessionCard";
+import assetsObject from "../../assets/assets";
+import ActiveSessionsPage from "./ActiveSessionsPage";
+import request_ressource from "../../utils/fetchApi";
+import { useState, useEffect } from "react";
 
-export default function Dev(){
+export default function Dev() {
+  const [data, setData] = useState([]);
+  const [status, setStatus] = useState("");
+  
+  async function getActiveSessionsFromServer() {
+    let url = "";
+    url = `http://localhost:8080/session/getAllActiveSessions`;
     
+    let response = null;
+    try {
+      response = await request_ressource(url, "GET");
+      setStatusCode(response.status);
+      response = await response.json();
+      setData(response);
+    } catch (e) {
+      console.log(e);
+    }
+  }
+  
+  useEffect(() => {
+    getActiveSessionsFromServer();
+  }, []);
 
-    return(<View>
-        <Text style={{ fontSize: 16, fontWeight: "bold", alignSelf: "center" }}>
-            ! FOR DEVELOPMENT ONLY !
-        </Text>
-        <StudySessionCard tag={"ECSE-324"} sessionTitle={"Studying Session"} sessionLocation={"Trottier"} numberOfAttendees={12} image={assetsObject.mcgillPhoto}/>
-    </View>)
+  let renderSessions = "";
+  renderSessions = data.map((event) => (
+    <View>
+      <StudySessionCard
+        tag={event.title}
+        sessionTitle={"Studying Session"}
+        sessionLocation={"Trottier"}
+        numberOfAttendees={12}
+        image={assetsObject.mcgillPhoto}
+      />
+    </View>
+  ));
+
+
+  return (
+    <View>
+      <Text style={{ fontSize: 16, fontWeight: "bold", alignSelf: "center" }}>
+        ! FOR DEVELOPMENT ONLY !
+      </Text>
+      {/* <StudySessionCard
+        tag={"ECSE-324"}
+        sessionTitle={"Studying Session"}
+        sessionLocation={"Trottier"}
+        numberOfAttendees={12}
+        image={assetsObject.mcgillPhoto}
+      /> */}
+      {renderSessions}
+    </View>
+  );
 }
--- frontend/utils/renderSessions.js
+++ frontend/utils/renderSessions.js
@@ -1,9 +1,11 @@
+import assetsObject from "../assets/assets";
+
 export default function renderSessions({ data }) {
   let renderSessions = "";
   renderSessions = data.map((event) => (
     <View>
       <StudySessionCard
-        tag={"ECSE-324"}
+        tag={event.title}
         sessionTitle={"Studying Session"}
         sessionLocation={"Trottier"}
         numberOfAttendees={12}
