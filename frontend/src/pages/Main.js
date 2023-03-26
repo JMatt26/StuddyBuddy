7a4797b1922f0baee17f322474912088879ca8af
Kristian Myzeqari <kristian.myzeqari@mail.mcgill.ca>
2023-03-26 16:56:57 -0400

    delete useless docs

--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -1,13 +1,36 @@
 import React, { Component } from "react";
+import { useState } from "react";
+
 import request_ressource from "../../utils/fetchApi";
 import ActiveSession from "../../components/ActiveSession";
-import { StyleSheet, View, Text, Button } from "react-native";export default function Main(){
-    let url = `http://localhost:8080/session/getAllActiveSessions`;
-    const response = request_ressource(url);
-    return(
-        response.array.forEach(element => {
-            <ActiveSession sessionName="${element.name}" location="${element.location}" ></ActiveSession>
-        });
-        
-    )
+import { StyleSheet, View, Text, Button } from "react-native";
+
+
+export default function Main(){
+    const [data, setData] = useState([]);
+    const [status, setStatus] = useState("");
+    let url = 'http://localhost:8080/session/getAllSessions';
+
+    let response = null;
+    try{
+        response = request_ressource(url);
+        setStatus(response.status);
+        response = response.body.json();
+        console.log(response);
+        setData(response);
+    } catch(e){
+        console.log(e.message);
+    }
+
+    let listSessions = data.map(element => {
+        return(
+            <View>
+                <ActiveSession
+                    sessionName={element.incoming.title}
+                    location={"Somewhere"}
+                    attendanceNbr={element.incoming.numberOfAttendees}>
+                </ActiveSession>
+            </View>
+        )
+    });  
 }
