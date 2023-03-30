77916e83380880d9f5b42b38c2fca480b5302c22
Kristian Myzeqari <kristian.myzeqari@mail.mcgill.ca>
2023-03-30 16:53:57 -0400

    Create active and upcoming components on home page

---
+++ frontend/components/ActiveSessionsComponent.js
@@ -0,0 +1,81 @@
+import request_ressource from "frontend/utils/fetchApi.js";
+import { useState, useEffect } from "react";
+import { isNil } from "frontend/utils/isNil.js";
+import React, { Component } from "react";
+import HomeCard from "./HomeCard";
+import { StyleSheet, ScrollView, View, Text, Button, Alert } from "react-native";
+
+export default function ActiveSessionsComponent() {
+  const [data, setData] = useState([]);
+  const [status, setStatus] = useState("");
+  
+  async function getAllActiveSessionsFromServer() {
+    let url = "";
+    url = `http://localhost:8080/session/getAllActiveSessions`;
+    
+    let response = null;
+    try {
+      response = await request_ressource(url, "GET");
+      response = await response.body.json();
+      console.log(response);
+      setData(response);
+    } catch (e) {
+      console.log(e);
+    }
+  }
+  
+  useEffect(() => {
+    getAllActiveSessionsFromServer();
+  }, []);
+  
+  const routeChange = () => {
+    Alert.alert("Session");
+  };
+
+  return (
+    <View>
+      <View style={styles.container}>
+          <Text style={styles.title}>Active Sessions</Text>
+          <Button onPress={routeChange} color="#0ead69" title="See All" />
+      </View>
+      <ScrollView horizontal={true}>
+      {data.map((event, index) => {
+        return(
+          <View key={index}>
+          <HomeCard 
+          sessionName={event.incoming.title}
+          location={isNil(event.incomingInfo?.location) ? null : event.incomingInfo.location} 
+          attendanceNbr={event.incoming.numberOfAttendees}/>
+          </View>
+        );
+      })}
+    </ScrollView>
+    </View>
+    
+  );
+
+}
+
+const styles = StyleSheet.create({
+    container: {
+      display: "flex",
+      flexDirection: "row",
+      justifyContent: "space-between",
+      marginTop: 10,
+    },
+    title: {
+      fontSize: 24,
+      marginHorizontal: 15,
+      marginTop: 5,
+    },
+    activeSessions: {
+        display: "flex",
+        flexDirection: "row",
+        flexWrap: "wrap",
+        alignSelf: "flex-start",
+        marginHorizontal: 15,
+        marginVertical: 5,
+        justifyContent: "space-between",
+    }
+  });
+
--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -1,5 +1,7 @@
 import { View } from "react-native";
- import AllSessionsComponent from "../../components/AllSessionsComponent";
+import AllSessionsComponent from "../../components/AllSessionsComponent";
+import ActiveSessionsComponent from "../../components/ActiveSessionsComponent";
+import UpcomingSessionsComponent from "../../components/UpcomingSessionsComponent";
 import request_ressource from "../../utils/fetchApi";
 import { useState, useEffect } from "react";
 import { isNil } from "../../utils/isNil";
@@ -9,6 +11,8 @@ export default function Main() {
   return (
     <View>
       <AllSessionsComponent />
+      <ActiveSessionsComponent />
+      <UpcomingSessionsComponent />
     </View>
   );
 }
